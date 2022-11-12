6a89a4b6fbd3d30137ec51dcb962cf7fb31c0f9d
jmsmg <jmsmg1@me.com>
2022-11-12 16:56:33 +0900

    prevent event

--- momentum/app.js
+++ momentum/app.js
@@ -1,1 +1,17 @@
-alert("hi"); 
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+
+const link = document.querySelector("a")
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    console.log(event);
+}
+
+function handleLinkClick(event) {
+    event.preventDefault();
+    console.log(event);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
